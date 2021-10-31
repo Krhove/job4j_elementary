e35d16a3eb7b49c387a51ade22ca0e44b45d0772
Khrove <alvum.main@gmail.com>
2021-10-31 13:48:31 +0300

    123

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -12,8 +12,8 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
+        float euro = Converter.rubleToEuro(1);
+        float dollar = Converter.rubleToDollar(1);
         System.out.println("140 rubles are " + euro + " euro or " + dollar + " dollars.");
     }
 }
